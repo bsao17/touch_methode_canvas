abe9182d120a5c43c0832a769389a4a712f21a1f
bsao17 <greta.b.meddhed>
2020-09-01 16:04:43 +0200

    Comments

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
+/**::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*
+ * Snippet code for used in canvas new methode of EC6 for 
+ * Touch devices 
+ * Mehddeb.b, devwebdino.com
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
+let el = document.querySelector("#canvas1");
+let context = el.getContext("2d");
+let onGoingTouches = new Array();
 
-  let el = document.querySelector('#canvas1')
-  let context = el.getContext('2d');
-  let onGoingTouches = new Array();
+el.addEventListener("touchmove", function (e) {
+  context.lineWidth = 1;
+  context.strokeStyle = "black";
+  var touches = e.changedTouches; //Object with a lot of position cursor
+  //First loop for first index
+  for (var i = 0; i < e.changedTouches.length; i++) {
+    console.log("Touch move start");
+    onGoingTouches.push(touches);
+    let idx;
+    //Second loop for other index
+    for (let x = 0; x < onGoingTouches.length; x++) {
+      idx = onGoingTouches[x];
+      context.lineTo(idx[i].pageX, idx[i].pageY);
+      context.stroke();
+    }
+    context.beginPath();
+  }
+});
 
-  
-  el.addEventListener('touchmove', function(e) {
-   
-    context.lineWidth = 1;
-    context.strokeStyle='black';
-    var touches = e.changedTouches;
-    
-    for (var i=0; i < e.changedTouches.length; i++) {
-        console.log("changedTouches[" + i + "].identifier = " + touches[i].identifier);
-        onGoingTouches.push(touches)
-        let idx;
-
-        
-        for(let x = 0; x < onGoingTouches.length; x++){
-            idx = onGoingTouches[x];
-            // console.log(idx[i].pageX, idx[i].pageY)
-            context.lineTo(idx[i].pageX, idx[i].pageY);
-            context.stroke(); 
-        }
-        context.beginPath(); 
-      
-    } 
-    
-   
-        
- }, false);
-
- /**------------------------------------------------------------------------------------------------------------------------- */
-
-
- 
+/**---------------------------------------------------------The End--;-)--------------------------------------------------------- */
