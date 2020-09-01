51677969be58185dae5adba7ad4716d2ee4c12da
bsao17 <greta.b.meddhed>
2020-09-01 16:09:21 +0200

    new comment

--- main.js
+++ main.js
@@ -6,20 +6,20 @@
 
 let el = document.querySelector("#canvas1");
 let context = el.getContext("2d");
-let onGoingTouches = new Array();
+let onTouches = new Array();
 
 el.addEventListener("touchmove", function (e) {
   context.lineWidth = 1;
   context.strokeStyle = "black";
-  var touches = e.changedTouches; //Object with a lot of position cursor
+  let touches = e.changedTouches; //Object with a lot of position cursor
   //First loop for first index
-  for (var i = 0; i < e.changedTouches.length; i++) {
+  for (let i = 0; i < e.changedTouches.length; i++) {
     console.log("Touch move start");
-    onGoingTouches.push(touches);
+    onTouches.push(touches);
     let idx;
     //Second loop for other index
-    for (let x = 0; x < onGoingTouches.length; x++) {
-      idx = onGoingTouches[x];
+    for (let x = 0; x < onTouches.length; x++) {
+      idx = onTouches[x];
       context.lineTo(idx[i].pageX, idx[i].pageY);
       context.stroke();
     }
