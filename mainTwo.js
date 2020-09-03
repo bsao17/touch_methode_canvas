37372c2961eebc0deebaa4f25a34ebd193b40d34
bsao17 <greta.b.meddhed>
2020-09-03 20:27:45 +0200

    comments

--- mainTwo.js
+++ mainTwo.js
@@ -1,27 +1,26 @@
-"use strict"
+"use strict";
 
 let el = document.querySelector("#canvas1");
 let context = el.getContext("2d");
-// context.lineWidth = "2"
-// context.strokeStyle = "black";
 
-el.addEventListener('touchstart', (e)=>{context.beginPath()})
-el.addEventListener('touchmove', makeSignature);
+el.addEventListener("touchstart", (e) => {
+  context.beginPath();
+});
+el.addEventListener("touchmove", makeSignature);
 
+function makeSignature(e) {
+  e.preventDefault();
+  const rect = event.target.getBoundingClientRect(); // target the event
+  console.log(e.touches);
+  let X = e.touches[0].clientX - rect.left;
+  let Y = e.touches[0].clientY - rect.top;
+  drawing(X, Y);
+  console.log(X, Y);
+}
 
-function makeSignature(e){
-    e.preventDefault();
-    const rect = event.target.getBoundingClientRect();
-    let X = e.touches[0].clientX - rect.left;
-    let Y = e.touches[0].clientY - rect.top;
-    drawing(X, Y);
-    console.log(X, Y)
-    
-};
-
-function drawing(x, y){
-     context.lineWidth = "2";
-     context.strokeStyle = "black";
-     context.lineTo(x, y);
-     context.stroke();
-};
+function drawing(x, y) {
+  context.lineWidth = "2";
+  context.strokeStyle = "black";
+  context.lineTo(x, y);
+  context.stroke();
+}
